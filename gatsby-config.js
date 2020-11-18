d84a5229ec9862fbe7bcb69c3118f9ca5bdf3393
unknown <hammad_205@hotmail.com>
2020-11-18 13:37:25 +0500

    fixed gatsbyconfig.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,7 +69,7 @@ module.exports = {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
-    `gatsby-plugin-feed`,
+
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- gatsby-node.js
+++ gatsby-node.js
@@ -6,16 +6,12 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   // Define a template for blog post
   const blogPost = path.resolve(`./src/templates/blog-post-content.js`)
 
-  // Get all markdown blog posts sorted by date
   const result = await graphql(
     `
     {
       allContentfulPost {
         edges {
           node {
-            title
-            subtitle
-            author
             slug
           }
         }
@@ -40,9 +36,6 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
   if (posts.length > 0) {
     posts.forEach((post, index) => {
-      const previousPostId = index === 0 ? null : posts[index - 1].id
-      const nextPostId = index === posts.length - 1 ? null : posts[index + 1].id
-
 
       createPage({
         path: `${post.node.slug}`,
@@ -57,44 +50,4 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
 
 
-// exports.createSchemaCustomization = ({ actions }) => {
-//   const { createTypes } = actions
 
-//   // Explicitly define the siteMetadata {} object
-//   // This way those will always be defined even if removed from gatsby-config.js
-
-//   // Also explicitly define the Markdown frontmatter
-//   // This way the "MarkdownRemark" queries will return `null` even when no
-//   // blog posts are stored inside "content/blog" instead of returning an error
-//   createTypes(`
-//     type SiteSiteMetadata {
-//       author: Author
-//       siteUrl: String
-//       social: Social
-//     }
-
-//     type Author {
-//       name: String
-//       summary: String
-//     }
-
-//     type Social {
-//       twitter: String
-//     }
-
-//     type MarkdownRemark implements Node {
-//       frontmatter: Frontmatter
-//       fields: Fields
-//     }
-
-//     type Frontmatter {
-//       title: String
-//       description: String
-//       date: Date @dateformat
-//     }
-
-//     type Fields {
-//       slug: String
-//     }
-//   `)
-// }
